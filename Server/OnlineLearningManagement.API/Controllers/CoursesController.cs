1564fe223a5eb057072d9f4610be722afac0d14a
Adi Nadler <adidi1k@gmail.com>
2025-07-22 12:28:22 +0300

    init solution

---
+++ Server/OnlineLearningManagement.API/Controllers/CoursesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLearningManagement.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CoursesController : ControllerBase
+	{
+	}
+}
